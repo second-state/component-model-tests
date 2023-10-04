9f24b3311b22f3fdbb07782de2e767fcbacd1844
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-10-04 15:21:34 +0800

    simplify the example
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- simple-run/src/lib.rs
+++ simple-run/src/lib.rs
@@ -5,8 +5,7 @@ use bindings::Guest;
 struct Component;
 
 impl Guest for Component {
-    /// Say hello!
-    fn hello_world() -> String {
-        "Hello, World!".to_string()
+    fn run() -> u32 {
+        0
     }
 }
