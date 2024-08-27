015585dbec536e51fd96d7b62960f2aaad571a8e
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2024-08-27 13:39:29 +0800

    update example, due to tool updation
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- add/src/lib.rs
+++ add/src/lib.rs
@@ -1,38 +1,14 @@
-cargo_component_bindings::generate!();
+#[allow(warnings)]
+mod bindings;
 
-use bindings::exports::component::add::types::{GuestEngine, Operation};
+use bindings::Guest;
 
-// Internal part
-use std::cell::RefCell;
+struct Component;
 
-pub struct Engine {
-    stack: RefCell<Vec<u32>>,
-}
-
-// GuestEngine 是要 export 的 resource Engine 的 trait
-impl GuestEngine for Engine {
-    fn new() -> Self {
-        Engine {
-            stack: RefCell::new(vec![]),
-        }
-    }
-
-    fn push_operand(&self, operand: u32) {
-        self.stack.borrow_mut().push(operand);
-    }
-
-    fn push_operation(&self, operation: Operation) {
-        let mut stack = self.stack.borrow_mut();
-        let right = stack.pop().unwrap(); // TODO: error handling!
-        let left = stack.pop().unwrap();
-        let result = match operation {
-            Operation::Add => left + right,
-            Operation::Sub => left - right,
-        };
-        stack.push(result);
-    }
-
-    fn execute(&self) -> u32 {
-        self.stack.borrow_mut().pop().unwrap() // TODO: error handling!
+impl Guest for Component {
+     fn add(x: i32, y: i32) -> i32 {
+        x + y
     }
 }
+
+bindings::export!(Component with_types_in bindings);
